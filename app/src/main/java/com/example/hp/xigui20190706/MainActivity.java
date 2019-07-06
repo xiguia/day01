ed25b6eec94fe4da1de42dfbff34b93778abea0a
xigui <1732520299@qq.com>
2019-07-06 11:32:05 +0800

    asdadsa

--- app/src/main/java/com/example/hp/xigui20190706/MainActivity.java
+++ app/src/main/java/com/example/hp/xigui20190706/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hp.xigui20190706;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "吐司", Toast.LENGTH_SHORT).show();
     }
 }
